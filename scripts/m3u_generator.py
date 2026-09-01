5047b7519df53572b32e25620cf724e6f7a0b5ca
Holden Lalumiere <102937506+HoldenLalumiere@users.noreply.github.com>
2026-09-01 16:48:35 -0400

    M3U generation now handles skipped files
    M3U generation used to skip (or add incorrectly) files that were skipped during download. The new implementation will record skipped files and add their paths to the M3U file correctly now.

--- scripts/m3u_generator.py
+++ scripts/m3u_generator.py
@@ -3,16 +3,22 @@ import os
 from scripts.utils import sanitize_filename, format_custom_filename
 
 
-def generate_m3u(collection_name, metadata_list, collection_path, file_ext, filename_lookup):
-	"""Generates a m3u file for the given collection"""
+def generate_m3u(collection_name, metadata_list, collection_path, file_ext, filename_lookup, path_overrides=None):
+	"""Generates a m3u file for the given collection. Can handle files outside of collection_path"""
+	path_overrides = path_overrides or {}
 	safe_collection_name = sanitize_filename(collection_name)
 	m3u_path = os.path.join(collection_path, f"{safe_collection_name}.m3u")
 
 	with open(m3u_path, "w", encoding="utf-8") as f:
 		f.write("#EXTM3U\n")
 		for track in metadata_list:
-			formatted_filename = filename_lookup[track["id"]]
-			filename = f"{formatted_filename}{file_ext}"
-			f.write(f"{filename}\n")
+			track_id = track["id"]
+			if track_id in path_overrides:
+				entry = os.path.relpath(path_overrides[track_id], start=collection_path)
+			else:
+				formatted_filename = filename_lookup[track_id]
+				entry = f"{formatted_filename}{file_ext}"
+
+			f.write(f"{entry}\n")
 
 	return m3u_path
